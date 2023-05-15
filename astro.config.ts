d1782a8fc3f2aad7500e7081fc79fda4f2cc4ea8
Sorami Hisamoto <hi@sorami.dev>
2023-05-15 19:28:09 +0900

    Revert "astro-relative-links"
    This reverts commit 5963f70650c78f8b0878a06778a54a1decf855bd.

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import relativeLinks from "astro-relative-links";
 
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
-    relativeLinks(),
   ],
 });
