6f3e06157437121539281729b81cb09712484a24
Sorami Hisamoto <hi@sorami.dev>
2023-05-20 16:15:47 +0900

    site title and description definition in a file

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
+import { SITE_TITLE, SITE_DESCRIPTION } from "../site";
+
 const allArticles = await getCollection("articles");
 const sortedArticles = allArticles.sort((a, b) => {
   return b.slug.localeCompare(a.slug);
@@ -8,9 +10,8 @@ const sortedArticles = allArticles.sort((a, b) => {
 
 export function get(context) {
   return rss({
-    title: "科学技術コミュ日記",
-    description:
-      "北海道で、CoSTEP「科学技術コミュニケーター養成プログラム」を受講する日々の記録です。",
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
     site: context.site,
     items: sortedArticles.map((article) => ({
       title: article.data.title,
---
+++ src/site.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "科学技術コミュ日記";
+export const SITE_DESCRIPTION =
+  "北海道で、CoSTEP「科学技術コミュニケーター養成プログラム」を受講する日々の記録です。";
